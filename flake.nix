5ec766aeb58800a67a0ed0739b9e06f7fe076919
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2026-06-28 05:50:38 +0900

    add nix ci checks

--- flake.nix
+++ flake.nix
@@ -8,17 +8,51 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      treefmt-nix,
+      ...
+    }:
     let
       system = "aarch64-darwin";
-      pkgs = import nixpkgs {
-        inherit system;
+      pkgs = import nixpkgs { inherit system; };
+      treefmtEval = treefmt-nix.lib.evalModule pkgs {
+        projectRootFile = "flake.nix";
+        programs.nixfmt = {
+          enable = true;
+          package = pkgs.nixfmt-rfc-style;
+        };
       };
     in
     {
+      formatter.${system} = treefmtEval.config.build.wrapper;
+
+      checks.${system} = {
+        format = treefmtEval.config.build.check self;
+
+        statix = pkgs.runCommand "statix-check" { nativeBuildInputs = [ pkgs.statix ]; } ''
+          cd ${self}
+          statix check .
+          touch $out
+        '';
+
+        deadnix = pkgs.runCommand "deadnix-check" { nativeBuildInputs = [ pkgs.deadnix ]; } ''
+          cd ${self}
+          deadnix --fail .
+          touch $out
+        '';
+      };
+
       homeConfigurations.kakudo = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
